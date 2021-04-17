6ccc29047e6f0be1163ead67e936165cbecc9a19
Karl Cooke <kcooke@outlook.com>
2021-04-17 05:24:02 +0100

    removing obsolete parameters

--- Platform/Scripts/staticWebsite.ps1
+++ Platform/Scripts/staticWebsite.ps1
@@ -1,4 +1,4 @@
-# Enabling the Static Website option for a Storage Account isn't exposed as a configurable item within Bicep or ARM Templates. We are using the below Azure CLI commands to enable this setting.
+# Enabling the Static Website option for a Storage Account isn't exposed as a configurable item within Bicep or ARM Templates. We are using the Azure CLI commands below to enable this setting.
 
 # Enable Static Website setting for Production Storage Account
 az storage blob service-properties update --account-name sairishtechieblogprod --static-website --404-document 404.html --index-document index.html
